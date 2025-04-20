907203d163c38cbff84333e6686dcaeaf27aa160
henrymeach <redsonjokii@gmail.com>
2025-04-20 21:33:00 +1000

    Make title header a link to the home page

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,15 @@
-import { Outlet } from "react-router";
-
+import { Link, Outlet } from "react-router";
+import '../App.css';
 
 export default function Layout() {
     return (
         <div>
             <header className="fixed top-2 right-1/2 translate-x-1/2">
-                <h1>
-                    raccoontype
-                </h1>
+                <Link to='/'>
+                    <h1 className="title text-3xl text-white">
+                        raccoon_type
+                    </h1>
+                </Link>
             </header>
             <main>
                 <Outlet />
