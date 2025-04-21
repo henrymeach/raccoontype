b48efd407306a0f3ed6c5bb72492c1741a8c37a3
henrymeach <redsonjokii@gmail.com>
2025-04-21 19:30:24 +1000

    Add footer and adjust header in layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,18 +2,28 @@ import { Link, Outlet } from "react-router";
 import '../App.css';
 
 export default function Layout() {
+    const year = new Date().getFullYear();
+
     return (
-        <div>
-            <header className="fixed top-2 right-1/2 translate-x-1/2">
-                <Link to='/'>
-                    <h1 className="title text-3xl text-white">
-                        raccoon_type
+        <div className="relative min-h-[100vh] flex flex-col">
+            <header className="mt-8 flex justify-center group">
+                <Link to='/' className="flex flex-row items-center">
+                    <div className="rounded outline-2 outline-[#e6e6e6] -outline-offset-2 w-8 h-8 flex items-center justify-center group-hover:scale-110 group-active:scale-90">
+                        <h1 className="title text-3xl">
+                            R
+                        </h1>
+                    </div>
+                    <h1 className="title text-3xl">
+                        accoon_Type
                     </h1>
                 </Link>
             </header>
-            <main>
+            <main className="px-[4%] md:px-[10%] my-auto">
                 <Outlet />
             </main>
+            <footer className="text-center mb-4 statistic-category">
+                © {year} — Henry Meach
+            </footer>
         </div>
     )
 }
