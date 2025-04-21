fcbd428f963b5d0925b07a0a2bb80b65801914cb
henrymeach <redsonjokii@gmail.com>
2025-04-21 19:29:38 +1000

    Add tooltip component

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -3,7 +3,7 @@ export default function Tooltip({title, className}: {
     className?: string;
 }) {
     return (
-        <div className={`${className} opacity-0 transition absolute text-sm -top-10 px-3 py-2 rounded bg-[rgb(230,230,230)]`}>
+        <div className={`${className} opacity-0 pointer-events-none transition absolute text-sm bottom-[105%] right-1/2 translate-x-1/2 px-3 py-2 w-[95%] rounded bg-[rgb(230,230,230)] group-hover:opacity-100 group-active:opacity-100`}>
             {title}
             <div className="absolute w-3 h-3 bg-[rgb(230,230,230)] right-1/2 translate-x-1/2 rotate-45" />
         </div>
