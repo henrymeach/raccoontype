079fd5d196fc73e9b7f614edf03232be7c77219b
henrymeach <redsonjokii@gmail.com>
2025-04-24 18:50:05 +1000

    Remove width restriction of tooltips

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -3,7 +3,7 @@ export default function Tooltip({title, className}: {
     className?: string;
 }) {
     return (
-        <div className={`${className} opacity-0 pointer-events-none transition absolute text-sm bottom-[105%] right-1/2 translate-x-1/2 px-3 py-2 w-[95%] rounded bg-[rgb(230,230,230)] group-hover:opacity-100 group-active:opacity-100`}>
+        <div className={`${className} opacity-0 pointer-events-none transition absolute text-sm bottom-[105%] right-1/2 translate-x-1/2 px-2 py-2 min-w-40 rounded bg-[rgb(230,230,230)] group-hover:opacity-100 group-active:opacity-100`}>
             {title}
             <div className="absolute w-3 h-3 bg-[rgb(230,230,230)] right-1/2 translate-x-1/2 rotate-45" />
         </div>
