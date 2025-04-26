b0270c68233cbb6ff28dd2a3476b1d07b0c8ee07
henrymeach <redsonjokii@gmail.com>
2025-04-26 22:38:13 +1000

    Add primitive leaderboard page

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,14 +6,18 @@ export default function Layout () {
 
   return (
     <div className='relative min-h-[100vh] flex flex-col'>
-      <header className='mt-8 flex justify-center'>
-        <Link to='/' className='flex flex-row items-center group'>
-          <div className='rounded outline-2 outline-[#e6e6e6] -outline-offset-2 w-8 h-8 flex items-center justify-center group-hover:scale-110 group-active:scale-90'>
-            <h1 className='title text-3xl'>R</h1>
-          </div>
-          <h1 className='title text-3xl'>accoon_Type</h1>
-        </Link>
+      <header className='relative mt-8 flex justify-center items-center mx-[4%] md:mx-[10%]'>
+          <Link to='/' className='flex flex-row items-center group'>
+            <div className='rounded outline-2 outline-[#e6e6e6] -outline-offset-2 w-8 h-8 flex items-center justify-center group-hover:scale-110 group-active:scale-90'>
+              <h1 className='title text-3xl'>R</h1>
+            </div>
+            <h1 className='title text-3xl'>accoon_Type</h1>
+          </Link>
+          <Link to='/leaderboard' className='absolute right-1' >
+            <img src='/icons/leaderboard.svg' />
+          </Link>
       </header>
+        
       <main className='px-[4%] md:px-[10%] my-auto'>
         <Outlet />
       </main>
--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import App from './App.tsx'
 import { BrowserRouter, Routes, Route } from 'react-router'
 import Layout from './components/Layout.tsx'
 import Finish from './pages/Finish.tsx'
+import Leaderboard from './pages/Leaderboard.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -13,6 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <Route path='/' element={<Layout />}>
           <Route index element={<App />} />
           <Route path='/finish' element={<Finish />} />
+          <Route path='/leaderboard' element={<Leaderboard />} />
         </Route>
       </Routes>
     </BrowserRouter>
