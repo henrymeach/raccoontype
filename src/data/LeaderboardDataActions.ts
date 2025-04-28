bbba2a065a8982577ddf99a6291b2e581ce377d4
henrymeach <redsonjokii@gmail.com>
2025-04-28 18:04:30 +1000

    Use correct supabase anon key

--- src/data/LeaderboardDataActions.ts
+++ src/data/LeaderboardDataActions.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_KEY);
+const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_ANON_KEY);
 
 export async function fetchLeaderboardData() {
     const { data, error } = await supabase
